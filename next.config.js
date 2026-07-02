690f5414879876fc52ccd1cc66426e47b6e6f8e4
habibClearview <habib@habibonifade.com>
2026-07-02 19:49:25 +0300

    Simplify next.config.js: remove withSentryConfig wrapper (not needed for error capture, only for source maps)

--- next.config.js
+++ next.config.js
@@ -1,18 +1,3 @@
-const { withSentryConfig } = require('@sentry/nextjs')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
-
-// Only wrap with Sentry if DSN is configured
-// This prevents build failures when Sentry env vars are not set
-if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
-  module.exports = withSentryConfig(nextConfig, {
-    silent: true,
-    widenClientFileUpload: true,
-    hideSourceMaps: true,
-    disableLogger: true,
-    automaticVercelMonitors: false,
-  })
-} else {
-  module.exports = nextConfig
-}
+module.exports = nextConfig
